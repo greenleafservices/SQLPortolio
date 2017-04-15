358c5b62a0224611471b6bb9cb19a2a75885d506
John Stegman <john@green-leaf-services.com>
2017-04-15 12:35:50 -0400

    Customized Registration URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: 'pages#home'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about', to: 'pages#about'
-
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
@@ -13,8 +14,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'angular-items', to: 'portfolios#angular'
+root to: 'pages#home'
 
 end
