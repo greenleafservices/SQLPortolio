ff1d69e3eb7db9333a81d12c0c680c26d5e5f74f
John Stegman <john@green-leaf-services.com>
2017-04-15 15:40:51 -0400

    Completed Authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first ##split will split the string at the first space or the supplied split point split(", ")
+  end
+
+  def last_name
+    self.name.split.last
+  end
+
 end
