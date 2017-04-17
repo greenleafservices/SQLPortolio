e0ad002f197cd95658b3172bea1f5f7e6c15743c
John Stegman <john@green-leaf-services.com>
2017-04-17 12:13:51 -0400

    Implemented view helper for login links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+   else
+     #we have to put these together in one string because Ruby will only act on the last item is a logic arguments and,
+     # without the concatenation of these two arguments, we'll never see the Register option
+      (link_to "Register ", new_user_registration_path) + "<br>".html_safe + (link_to " Login", new_user_session_path)
+   end
+  end
+
 end
