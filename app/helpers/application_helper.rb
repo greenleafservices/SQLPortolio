6a5d77d6b301b4eed7aeefeb4cb7c1b2fd213408
John Stegman <john@green-leaf-services.com>
2017-04-20 16:28:47 -0400

    Updated master layout styles and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,16 @@ module ApplicationHelper
   #before_action:set_copyright
   #before_action:set_repeatcopyright
 
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
       #we have to put these together in one string because Ruby will only act on the last item is a logic arguments and,
       # without the concatenation of these two arguments, we'll never see the Register option
-       (link_to "Register ", new_user_registration_path) + "<br>".html_safe + (link_to " Login", new_user_session_path)
+       (link_to "Register ", new_user_registration_path, class: style) + " ".html_safe + (link_to " Login", new_user_session_path, class: style)
     else
-        link_to "Logout", destroy_user_session_path, method: :delete
+        link_to "Logout", destroy_user_session_path, method: :delete, class: style
    end
   end
-  
+
   def source_helper(layout_name)
       if session[:source]
         greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
