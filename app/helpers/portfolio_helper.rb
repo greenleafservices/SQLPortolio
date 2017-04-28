93452aa4564dc25a555e2ed15f7ee6796f070dd1
John Stegman <john@green-leaf-services.com>
2017-04-28 11:16:11 -0400

    04-28-17

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -3,7 +3,7 @@ module PortfolioHelper
   def image_generator(height:, width:)
     "http://placehold.it/#{height}x#{width}"
   end
-  
+
   def portfolio_img img, type
       if img.model.main_image? || img.model.thumb_image?
         img
